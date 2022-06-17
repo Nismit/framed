49e9d918ca65129a48d5dc1ef964e9c21b3c4ee1
Nismit <nismit.dev@gmail.com>
2022-06-17 00:13:18 -0700

    Update UI, improve performance

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
-import { format } from "date-fns";
+import { format, startOfDay, add, isAfter } from "date-fns";
 import { Time } from "./components/Time";
-import { useThree } from "./hooks/useThree";
+import { Canvas } from "./components/Canvas";
 import { useWeather } from "./hooks/useWeather";
 import { weatherIconMapping } from "./components/utils";
 import { WeatherIconComponents } from "./icons";
@@ -9,11 +9,10 @@ import { WeatherIconComponents } from "./icons";
 
 export function App() {
   const { location, forecasts } = useWeather();
-  const { threeRef } = useThree();
 
   return (
     <>
-      <div ref={threeRef} className="canvasContainer" />
+      <Canvas />
       <div className="info">
         <div className="forecast">
           {forecasts &&
@@ -23,11 +22,19 @@ export function App() {
               }
 
               const { id, description } = forecast.weather[0];
-              const iconId = `Night${weatherIconMapping[id]}`;
+              const evening = add(startOfDay(new Date(forecast.dt * 1000)), {
+                hours: 18,
+              });
+              const isNight = isAfter(new Date(forecast.dt * 1000), evening);
+              const iconId = `${isNight ? "Night" : "Day"}${
+                weatherIconMapping[id]
+              }`;
               return (
                 <div className="weather" key={index}>
                   {WeatherIconComponents[iconId]}
-                  <span>{description}</span>
+                  <span>
+                    {forecast.main.temp_min}℃/{forecast.main.temp_max}℃
+                  </span>
                   <span>{format(new Date(forecast.dt * 1000), "p")}</span>
                 </div>
               );
