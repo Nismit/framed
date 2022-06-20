eba850e0de13a4c66db93c001ab43281dd9c6fe9
Nismit <nismit.dev@gmail.com>
2022-06-19 19:01:49 -0700

    Still test

--- src/hooks/useThree.ts
+++ src/hooks/useThree.ts
@@ -2,9 +2,9 @@ import { useRef, useEffect, useState, useCallback } from "preact/hooks";
 import { Scene, PerspectiveCamera, WebGLRenderer, Vector2 } from "three";
 import Stats from "three/examples/jsm/libs/stats.module";
 import { useEventListener } from "./useEventListener";
-import { boxObject } from "../utils/boxObject";
-// import baseMesh from "../utils/baseMesh";
-// import fragment1 from "../fragments/fbm.frag";
+// import { boxObject } from "../utils/boxObject";
+import baseMesh from "../utils/baseMesh";
+import fragment1 from "../fragments/fbm.frag";
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(75, 1, 0.1, 10);
@@ -15,28 +15,28 @@ document.body.appendChild(stats.dom);
 
 // Config
 camera.position.z = 3;
-const cube = boxObject();
+// const cube = boxObject();
 
-// const baseObject = new baseMesh({
-//   fragment: fragment1,
-//   uniform: {
-//     pixelRatio: {
-//       value: window.devicePixelRatio,
-//     },
-//     resolution: {
-//       value: new Vector2(),
-//     },
-//     time: {
-//       value: 0,
-//     },
-//     // offset: {
-//     //   value: 0.75,
-//     // },
-//     seed: {
-//       value: new Vector2(-0.345, 0.654),
-//     },
-//   },
-// });
+const baseObject = new baseMesh({
+  fragment: fragment1,
+  uniform: {
+    pixelRatio: {
+      value: window.devicePixelRatio,
+    },
+    resolution: {
+      value: new Vector2(),
+    },
+    time: {
+      value: 0,
+    },
+    // offset: {
+    //   value: 0.75,
+    // },
+    seed: {
+      value: new Vector2(-0.345, 0.654),
+    },
+  },
+});
 
 export const useThree = () => {
   const rafRef = useRef<number>();
@@ -50,7 +50,7 @@ export const useThree = () => {
       const height = threeRef.current.clientHeight;
 
       // sketch.resolution = { x: width, y: height };
-      // baseObject.resolution = { x: width, y: height };
+      baseObject.resolution = { x: width, y: height };
 
       renderer.setSize(width, height);
       renderer.setPixelRatio(window.devicePixelRatio);
@@ -61,10 +61,10 @@ export const useThree = () => {
   };
 
   const render = (frame?: number) => {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
     // sketch.time = frame ?? time;
-    // baseObject.time = frame ?? time;
+    baseObject.time = frame ?? time;
     renderer.render(scene, camera);
     stats.update();
   };
@@ -75,19 +75,19 @@ export const useThree = () => {
     if (threeRef.current) {
       resizeHandler();
       threeRef.current.appendChild(renderer.domElement);
-      scene.add(cube);
+      // scene.add(cube);
       // scene.add(sketch.mesh);
-      // scene.add(baseObject.mesh);
+      scene.add(baseObject.mesh);
     }
 
     return () => {
       if (threeRef.current) {
         // sketch.dispose();
         // scene.remove(sketch.mesh);
-        scene.remove(cube);
+        // scene.remove(cube);
         // cube.remove();
-        // baseObject.dispose();
-        // scene.remove(baseObject.mesh);
+        baseObject.dispose();
+        scene.remove(baseObject.mesh);
         renderer.dispose();
         threeRef.current.removeChild(renderer.domElement);
       }
