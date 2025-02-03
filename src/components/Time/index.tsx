71d6a78dd56e279c84b0be2111eaa78296fe39b2
Nismit <22581499+Nismit@users.noreply.github.com>
2025-02-02 17:41:23 -0800

    chore: bump ts version and fix related stuff

--- src/components/Time/index.tsx
+++ src/components/Time/index.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "preact";
 import { format } from "date-fns";
 import { useCurrentDate } from "../../hooks/useCurrentDate";
 
@@ -5,11 +6,11 @@ export const Time = () => {
   const date = useCurrentDate(1000);
 
   return (
-    <>
+    <Fragment>
       <p className="location date">{format(date, "MM/dd - EEEE")}</p>
       <div className="time">
         <p>{date.toLocaleTimeString()}</p>
       </div>
-    </>
+    </Fragment>
   );
 };
